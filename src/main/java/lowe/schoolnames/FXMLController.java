c23bfcb6bda1b14b4f2c4c0f32968456d20a66bb
ethanl82 <ethanl82@BHSCUR149020SPW.kprdsb.schools.local>
2021-11-25 14:25:29 -0500

    Everything but Averages

--- src/main/java/lowe/schoolnames/FXMLController.java
+++ src/main/java/lowe/schoolnames/FXMLController.java
@@ -11,16 +11,65 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class FXMLController implements Initializable {
     
     @FXML
-    private Label lblOut;
+    private TextField txtTest1;
+
+    @FXML
+    private TextField txtTest2;
+
+    @FXML
+    private TextField txtTest3;
+
+    @FXML
+    private TextField txtTest4;
+
+    @FXML
+    private TextField txtFirst;
+
+    @FXML
+    private Label lblStudent;
+    
     
     @FXML
-    private void btnClickAction(ActionEvent event) {
-        lblOut.setText("Hello World!");
+    private TextField txtLast;
+    
+    String Spots [][] = new String[15][6];
+    
+    int StudentNumber = 0; 
+
+    @FXML
+    void btnAdd(ActionEvent event) {
+        Spots[StudentNumber][0]= ""+txtFirst;
+        Spots[StudentNumber][1]= ""+txtLast;
+        Spots[StudentNumber][2]= ""+txtTest1;
+        Spots[StudentNumber][3]= ""+txtTest2;
+        Spots[StudentNumber][4]= ""+txtTest3;
+        Spots[StudentNumber][5]= ""+txtTest4;
+        StudentNumber++;
     }
+
+    @FXML
+    void btnCAvrg(ActionEvent event) {
+        
+    }
+
+    @FXML
+    void btnList(ActionEvent event) {
+        String output = "";
+        for (int i=0; i <StudentNumber;i++){
+            output=output +"Name: "+Spots[i][0]+ " "+ Spots[i][1]+" "+ Spots[i][2]+" "+ Spots[i][3]+" "+ Spots[i][4]+" "+ Spots[i][5] +"/n";
+        }
+    }
+
+    @FXML
+    void btnSAvrg(ActionEvent event) {
+        
+    }
+
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
